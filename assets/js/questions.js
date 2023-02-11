3524b7e0e1830dfeb93f58fdae8a524f8c1e7f08
jh871 <jennyh871@gmail.com>
2023-02-11 21:07:56 +0000

    added copy questions below placeholders, commented out

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -26,3 +26,105 @@ let questionPool = [
     // slice, foreach, 2d array
     },
 ]
+
+
+/*
+let questions = [
+    {
+        question: "What's your favourite season?",
+        answers: [
+            "Spring",
+            "Summer",
+            "Autumn",
+            "Winter"
+        ],
+    },
+    {
+        question: "What's your favourite colour?",
+        answers: [
+            "Yellow",
+            "Green",
+            "Red",
+            "Blue"
+        ],
+    },
+    {
+        question: "On a night out with you your best mate gets in a fight. Where are you?",
+        answers: [
+            "Throwing the first punch",
+            "Leading peace talks",
+            "Running back into the bar and hiding in the toilets",
+            "Grabbing the popcorn"
+        ],
+    },
+    {
+        question: "In the training montage of your life, what's the sountrack?",
+        answers: [
+            "Classic rock",
+            "Slow, pensive indie-folk",
+            "Hyperpop remixes of songs that were already pretty hyper",
+            "Ballads!"
+        ],
+    },
+    {
+        question: "What would you rather have handy if you're stranded on a desert island?",
+        answers: [
+            "A stack of books you'll finally have time to get through",
+            "Swimwear, beachball, sunlotion",
+            "A survival guide and a first aid kit",
+            "A few of your closest friends"
+        ],
+    },
+    {
+        question: "What's your dream holiday destination?",
+        answers: [
+            "Snowy mountains",
+            "In the city",
+            "A nice cabin in a forest",
+            "Beachside"
+        ],
+    },
+    {
+        question: "You have to survive on just one of the below foods for the next year. What's your pick?",
+        answers: [
+            "Dessert",
+            "Plant-based everything",
+            "Pizza",
+            "Can a person survive on cheese alone? Let's find out!"
+        ],
+    },
+    {
+        question: "Who were you in high school?",
+        answers: [
+            "The class clown/detention magnet",
+            "Everyone's friend",
+            "On every sport's team",
+            "Top of the class"
+        ],
+    },
+    {
+        question: "It's your pick for movie night. What are we watching?",
+        answers: [
+            "A good comedy or cheesy rom-com",
+            "Something older than your parents",
+            "Action! (get it?)",
+            "A horror so creepy that even you get nightmares"
+        ],
+    }
+]
+*/
+
+
+//Template to add new question:
+/*
+,
+    {
+        question: "",
+        answers: [
+            "",
+            "",
+            "",
+            ""
+        ],
+    }
+*/
