3697cf5b9208d7230baee326b24822b75c077596
jh871 <jennyh871@gmail.com>
2023-02-11 23:14:41 +0000

    added id to returned gif

--- assets/js/results.js
+++ assets/js/results.js
@@ -6,43 +6,43 @@ let gifReturn = $("#gif-return");
 
 // $("body").append(displayDiv);
 
-function pokemonSearch() {
-  var queryURL = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    // console.log(response);
-    let pokemon = response.name;
+// function pokemonSearch() {
+//   var queryURL = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET",
+//   }).then(function (response) {
+//     // console.log(response);
+//     let pokemon = response.name;
 
-    let pPokemon = $("<p>");
-    pPokemon.text("you are " + pokemon);
+//     let pPokemon = $("<p>");
+//     pPokemon.text("you are " + pokemon);
 
-    let indexNo = response.game_indices[12].game_index;
-    let pIndex = $("<p>");
-    pIndex.text("#" + indexNo);
+//     let indexNo = response.game_indices[12].game_index;
+//     let pIndex = $("<p>");
+//     pIndex.text("#" + indexNo);
 
-    let pokemonType = response.types[0].type.name;
-    let pType = $("<p>");
-    pType.text("type: " + pokemonType);
+//     let pokemonType = response.types[0].type.name;
+//     let pType = $("<p>");
+//     pType.text("type: " + pokemonType);
 
-    let pokemonAbility = response.abilities[1].ability.name;
-    let pAbility = $("<p>");
-    pAbility.text("Ability: " + pokemonAbility);
+//     let pokemonAbility = response.abilities[1].ability.name;
+//     let pAbility = $("<p>");
+//     pAbility.text("Ability: " + pokemonAbility);
 
-    pokeInfo.append(pPokemon);
-    pokeInfo.append(pIndex);
-    pokeInfo.append(pType);
-    pokeInfo.append(pAbility);
-  });
+//     pokeInfo.append(pPokemon);
+//     pokeInfo.append(pIndex);
+//     pokeInfo.append(pType);
+//     pokeInfo.append(pAbility);
+//   });
   /*
     pokeInfo.append(pPokemon);
     pokeInfo.append(pIndex);
     pokeInfo.append(pType);
     pokeInfo.append(pAbility);*/
-}
+// }
 
-pokemonSearch();
+// pokemonSearch();
 
 let gifTag = $("<img>");
 function gifSearch() {
@@ -59,7 +59,10 @@ function gifSearch() {
     // console.log(response.data[0].images.fixed_height.url);
     var gifURL = response.data[0].images.fixed_height.url;
     //
-    gifTag.attr("src", gifURL);
+    gifTag.attr({
+        "src":gifURL,
+        "id":"gif-img"
+        });
     gifReturn.append(gifTag);
   });
 }
@@ -67,40 +70,40 @@ gifSearch();
 
 
 
-const APIKey = `qEfp8gLbO9z879S740uTEQeF`;
-const limit = `&limit=50`;
-let pokeNames = [];
-let randomPokeName = $(`#random-pokename`);
-let generatorURL =
-  `https://api.fungenerators.com/name/generate?category=pokemon` +
-  limit +
-  `&api_key=` +
-  APIKey;
+// const APIKey = `qEfp8gLbO9z879S740uTEQeF`;
+// const limit = `&limit=50`;
+// let pokeNames = [];
+// let randomPokeName = $(`#random-pokename`);
+// let generatorURL =
+//   `https://api.fungenerators.com/name/generate?category=pokemon` +
+//   limit +
+//   `&api_key=` +
+//   APIKey;
 
-$.ajax({
-  url: generatorURL,
-  method: `GET`,
-}).then(function (response) {
-  console.log(response);
-  let names = response.contents.names;
-  console.log(names);
-  for (let i = 0; i < names.length; i++)
-    if (pokeNames.includes(names[i])) {
-      return;
-    } else {
-      pokeNames.push(names[i]);
-    }
-  let nameDisplay = $(`<h1>`);
-  nameDisplay.text(`Your pokename is` + ` ` + getRandom());
-  randomPokeName.append(nameDisplay);
-})
+// $.ajax({
+//   url: generatorURL,
+//   method: `GET`,
+// }).then(function (response) {
+//   console.log(response);
+//   let names = response.contents.names;
+//   console.log(names);
+//   for (let i = 0; i < names.length; i++)
+//     if (pokeNames.includes(names[i])) {
+//       return;
+//     } else {
+//       pokeNames.push(names[i]);
+//     }
+//   let nameDisplay = $(`<h1>`);
+//   nameDisplay.text(`Your pokename is` + ` ` + getRandom());
+//   randomPokeName.append(nameDisplay);
+// })
 
 
-function getRandom(response) {
-  let randomName = [];
-  randomName.push(pokeNames[Math.floor(Math.random() * pokeNames.length)]);
-  return randomName;
-}
+// function getRandom(response) {
+//   let randomName = [];
+//   randomName.push(pokeNames[Math.floor(Math.random() * pokeNames.length)]);
+//   return randomName;
+// }
 
 //FORM for certificate//
 
