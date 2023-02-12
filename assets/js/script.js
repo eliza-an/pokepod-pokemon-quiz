9d1743d15d1e6779208fbdb4cef576481997de8b
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-02-12 12:51:18 +0000

    added html pages for each question and changed the ids to match the question pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,10 @@ const startBtn = document.querySelector(`#startBtn`);
 const question = document.querySelector(`#question`);
 const questionTitle = document.querySelector(`#qTitle`);
 const options = document.querySelector(`#options`);
+
+
+let snorlax=0;
+let pikachu=0;
+let bulbasaur=0;
+let squirtle=0;
+let charmander=0;
